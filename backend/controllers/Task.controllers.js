a6271ebca23bc4f737c14264b3d01d176fbe19b2
alok <alokmishra7464@gmail.com>
2026-03-01 09:54:19 +0530

    added deleteTask functionality in task controller

--- backend/controllers/Task.controllers.js
+++ backend/controllers/Task.controllers.js
@@ -1,6 +1,5 @@
 import { Task } from "../models/Task.models.js";
 import { Board } from "../models/Board.models.js";
-import { User } from "../models/User.models.js";
 import { Column } from "../models/Column.models.js";
 
 //1. create tasks
@@ -49,6 +48,36 @@ export const createTask = async(req, res) => {
             order: taskCount,
         });
         res.status(201).json(task);
+    }
+    catch(error) {
+        res.status(500).json({ message: error.message});
+    }
+};
+
+// delete task
+export const deleteTask = async(req, res) => {
+    try {
+        const { taskId } = req.params;
+        const task = await Task.findById(taskId);
+        if(!task) {
+            return res.status(404).json({ message: "Task not found"});
+        }
+        
+        const board = await Board.findById(task.board);
+        if(!board) {
+            return res.status(404).json({ message: "Board not found"});
+        }
+        const isMember = board.members.some(
+            (member) => req.user._id.toString() === member.toString()
+        );
+        if(!isMember) {
+            return res.stauts(403).json({ message: "Not Authorized to delete task"});
+        }
+
+        await task.deleteOne();
+
+        res.status(200).json({ message: "Task Deleted!!!"});
+
     }
     catch(error) {
         res.status(500).json({ message: error.message});
